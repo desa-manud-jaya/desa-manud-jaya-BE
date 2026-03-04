05041c00706cfa454532f39d0c16451070884df4
itopnaibaho51 <christopher.moses51@ui.ac.id>
2026-03-04 22:11:32 +0700

    initial project

---
+++ src/main/java/com/example/manud_jaya/ManudJayaApplication.java
@@ -0,0 +1,13 @@
+package com.example.manud_jaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManudJayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManudJayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/manud_jaya/ManudJayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manud_jaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManudJayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
